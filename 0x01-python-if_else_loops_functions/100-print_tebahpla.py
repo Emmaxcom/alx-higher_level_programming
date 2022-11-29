cfb6d9f4ad4cd2a1b52ed9d8ce1171cad339c2d5
Emmaxcom <emmaxcom@yahoo.com>
2022-11-29 13:40:45 -0800

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for a in range(122, 96, -1):
-    if(a % 2 != 0):
-        a -= 32
-        print("{:s}".format(chr(a)), end="")
+for i in 'zYxWvUtSrQpOnMlKjIhGfEdCbA':
+    print("{}".format(i), end="")
